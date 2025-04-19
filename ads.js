5a6703cd53831711dce05fbdc6a9947075c4b87c
DevEmmanuel331 <alhassanemmanuelazong@gmail.com>
2025-04-19 15:05:28 +0000

    update

---
+++ ads.js
@@ -0,0 +1,49 @@
+/* 
+  document.addEventListener("DOMContentLoaded", function() {
+    const adSpace = document.getElementById('ad-container1');
+    adSpace.innerHTML = `
+      <div style=" word-break: break-word; overflow-wrap: break-word; padding: 10px; width :100%; background: #f1f8e9; border: 1px solid #7cb342;"
+           width :100%; background: #f1f8e9; border: 1px solid #7cb342;">
+          <h3>7777!</h3>
+          <p>333333333</p>
+          <a href="https://your-default-offer.com" target="_blank">Discover Now</a>
+        </div>
+    `;
+  });
+  
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const adSpace = document.getElementById('ad-container2');
+    adSpace.innerHTML = `
+     <div style=" word-break: break-word; overflow-wrap: break-word; padding: 10px; width :100%; background: #f1f8e9; border: 1px solid #7cb342;"
+           width :100%; background: #f1f8e9; border: 1px solid #7cb342;">
+          <h3>7777!</h3>
+          <p>333333333333333333333333333333333333333333</p>
+          <a href="https://your-default-offer.com" target="_blank">Discover Now</a>
+        </div>
+    `;
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const adSpace = document.getElementById('ad-container3');
+    adSpace.innerHTML = `
+     <div style=" word-break: break-word; overflow-wrap: break-word; padding: 10px; width :100%; background: #f1f8e9; border: 1px solid #7cb342;"
+           width :100%; background: #f1f8e9; border: 1px solid #7cb342;">
+          <h3>7777!</h3>
+          <p>333333333333333333333333333333333333333333</p>
+          <a href="https://your-default-offer.com" target="_blank">Discover Now</a>
+        </div>
+    `;
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const adSpace = document.getElementById('ad-container4');
+    adSpace.innerHTML = `
+     <div style=" word-break: break-word; overflow-wrap: break-word; padding: 10px; width :100%; background: #f1f8e9; border: 1px solid #7cb342;"
+           width :100%; background: #f1f8e9; border: 1px solid #7cb342;">
+          <h3>7777!</h3>
+          <p>333333333333333333333333333333333333333333</p>
+          <a href="https://your-default-offer.com" target="_blank">Discover Now</a>
+        </div>
+    `;
+  });   */
