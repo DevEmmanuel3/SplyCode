75725e0cd357811d9328065616289a7f2c6309e7
DevEmmanuel331 <alhassanemmanuelazong@gmail.com>
2025-06-10 11:55:33 +0000

    new

--- Cjs/blog.js
+++ Cjs/blog.js
@@ -36,7 +36,7 @@ if (document.getElementById("blogPosts")) {
       a.innerHTML = `
 
 
-                        <div class="col-lg-6" style="width:80%; justify-content: center; margin:10px;  word-break:break-word; overflow-wrap:break-word;">
+                        <div class="mx-auto"  style="width:80%; justify-content: center; margin:10px;  word-break:break-word; overflow-wrap:break-word;">
                         <div class="position-relative mb-3">
                             <img class="img-fluid w-100" src="${post.image}" style="object-fit: cover;" alt="${post.title}" >
 <div class="overlay position-relative bg-light" style="max-width: 100%; overflow: hidden;">
